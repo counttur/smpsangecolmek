f032229a33700cc293b78d1b84a01149f68d5ab5
counttur <counttur@gmail.com>
2025-07-10 19:48:26 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# smpsangecolmek
-smpsangecolmek
+# [smpsangecolmek](https://smpsangecolmek.pages.dev)
---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const url = 'https://smpsangecolmek.pages.dev';
+export const nama = 'Smp Sange Colmek';
+export const desk = 'Video Bokep Indo Terbaru Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante';
+export const terbit = '2025-01-25T00:01:21Z';
+
+export const google = 'Vx9au_qEDcmjB-53MaQ8-1jiNpsXXUyltTZXBHYX18o';
+export const yandex = '6061fb4d883b46fd';
+
+export const index = 'dcc81d87-738c-4446-bd57-8f3ede05b09c';
